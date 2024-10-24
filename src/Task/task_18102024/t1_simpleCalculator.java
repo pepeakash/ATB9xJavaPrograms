86ef01bfeffac6b7dfa866f8e70195c42c35859f
pepeakash <56559814+pepeakash@users.noreply.github.com>
2024-10-24 23:06:44 +0530

    24/10/2024 changes

---
+++ src/Task/task_18102024/t1_simpleCalculator.java
@@ -0,0 +1,58 @@
+package Task.task_18102024;
+
+import java.util.Scanner;
+
+public class t1_simpleCalculator {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter first number:");
+            double num1 = sc.nextDouble();
+
+            System.out.println("Enter second number:");
+            double num2 = sc.nextDouble();
+
+            System.out.println("Enter an operator (+, -, *, /, %):");
+            char operator = sc.next().charAt(0);
+
+            double result;
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                        System.out.println("Result: " + result);
+                    } else {
+                        System.out.println("Error: Division by zero");
+                    }
+                    break;
+                case '%':
+                    if (num2 != 0) {
+                        result = num1 % num2;
+                        System.out.println("Result: " + result);
+                    } else {
+                        System.out.println("Error: Division by zero");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operator");
+                    break;
+            }
+
+            sc.close();
+        }
+    }
+
