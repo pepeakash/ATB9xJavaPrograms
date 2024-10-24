05cc539bb4425ec73143546b018e7e508349e199
pepeakash <56559814+pepeakash@users.noreply.github.com>
2024-10-24 20:18:56 +0530

    24/10/2024 changes

--- src/Task/task_03102024/Task2.java
+++ src/Task/task_03102024/Task2.java
@@ -3,12 +3,16 @@ package Task.task_03102024;
 public class Task2 {
     public static void main(String[] args) {
         // Table a 10  - sop
-        System.out.println("10x1=10");
-        System.out.println("10x2=20");
-        System.out.println("10x3=30");
-        System.out.println("10x4=40");
-        System.out.println("...");
-        System.out.println("10x10=100");
-
+        System.out.println("10 x 1 = " + 10 * 1);
+        System.out.println("10 x 2 = " + 10 * 2);
+        System.out.println("10 x 3 = " + 10 * 3);
+        System.out.println("10 x 4 = " + 10 * 4);
+        System.out.println("10 x 5 = " + 10 * 5);
+        System.out.println("10 x 6 = " + 10 * 6);
+        System.out.println("10 x 7 = " + 10 * 7);
+        System.out.println("10 x 8 = " + 10 * 8);
+        System.out.println("10 x 9 = " + 10 * 9);
+        System.out.println("10 x 10 = " + 10 * 10);
     }
 }
+
--- src/Task/task_03102024/Task3.java
+++ src/Task/task_03102024/Task3.java
@@ -2,10 +2,25 @@ package Task.task_03102024;
 
 public class Task3 {
     public static void main(String[] args) {
-        //
-        System.out.println(10+3); // Add
-        System.out.println(10-3); // Sub
-        System.out.println(10*3); // Mul
-        System.out.println(10/3);  // Div
+        System.out.println("Addition of numbers: "+ add(3,4));
+        System.out.println("Subtraction of numbers: "+ subtract(13,4));
+        System.out.println("Division of numbers: "+ divide(30,5));
+        System.out.println("Multiplication of numbers: "+ multiply(3,4));
+    }
+
+    public static int add(int a,int b){
+        return a+b;
+    }
+
+    public static int subtract(int a,int b){
+        return a-b;
+    }
+
+    public static int divide(int a,int b){
+        return a/b;
+    }
+
+    public static int multiply(int a,int b){
+        return a*b;
     }
 }
